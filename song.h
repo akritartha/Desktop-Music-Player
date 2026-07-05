07e13a473c73ccd10d777c8d1e6342e1f74b3f4d
Kritartha Adhikari <kritarthadybala10@gmail.com>
2026-07-05 23:13:35 +0545

    Audio files update

---
+++ song.h
@@ -0,0 +1,13 @@
+//Declaration of what the class looks like
+#pragma once
+#include <QString>
+class Song{
+    private:
+        QString title;
+        QString artist;
+    public:
+        Song(QString title, QString artist);
+        QString 
+
+
+}
