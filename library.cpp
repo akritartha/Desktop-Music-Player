f41ea330d67b9beea8473bb5b67453a13b8e6ec2
Kritartha Adhikari <kritarthadybala10@gmail.com>
2026-07-12 19:59:57 +0545

    Remove songs from library

--- library.cpp
+++ library.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "library.h"
+#include <fstream>
 using namespace std;
 Library::Library(){}
 Library::Library(string dbPath):m_dbPath(dbPath){
@@ -17,5 +18,25 @@ bool Library::addSong(const Song& song){
     }
     m_songs.push_back(song); //appends song to the end of the vector
     return true;
+}
+bool Library::save(){
+    ofstream file(m_dbPath); //output file stream for writing
+    if(!file.is_open()){
+        return false;
+    }
+    for (const auto& s:m_songs){
+        file<<s.id()<<"|"<<s.title()<<"|"
+        <<s.artist()<<"|"<<s.album()<<"|"
+        <<s.filepath()<<"|"<<s.duration()<<"\n";
+    }
+}
+bool Library::removeSong(int id){
+    for (int i=0;i<(int)m_songs.size();i++){ //.size() returns size_t type
+        if(m_songs[i].id()==id){
+            m_songs.erase(m_songs.begin()+i);
+            return true;
+        }
+    }
+    return false;
 }
 
